a88979222a511ed790c9ec56b7130d869b536046
Prague Fashion Photographer <info@davebrownphotography.com>
2017-04-22 20:02:06 +0200

    checking in documents

--- src/controllers/base/base.controller.ts
+++ src/controllers/base/base.controller.ts
@@ -1,19 +1,16 @@
-import { Router, Request, Response, RequestParamHandler, NextFunction, RequestHandler } from 'express';
+import { NextFunction, Request, RequestHandler, RequestParamHandler, Response, Router } from 'express';
 import mongoose = require('mongoose');
-import { Schema, Model, Document, DocumentQuery } from 'mongoose';
-import { SearchCriteria } from "../../models/search-criteria";
+import { Document, DocumentQuery, Model, Schema } from 'mongoose';
+import { SearchCriteria } from '../../models/search-criteria';
 import log = require('winston');
 
-//Model<IUser> & IUser
-//OLD WAY: BaseController<ModelType extends Document>
-//export abstract class BaseController<T extends Document,XType extends Document & Model<T>>{
+// Model<IUser> & IUser
+// OLD WAY: BaseController<ModelType extends Document>
+// export abstract class BaseController<T extends Document,XType extends Document & Model<T>>{
 export abstract class BaseController<IModelMongooseComposite extends Document>{
   public mongooseModelInstance: Model<IModelMongooseComposite>;
-  searchCriteria: SearchCriteria;
-  abstract defaultPopulationArgument: Object;
-
-  public constructor() {
-  }
+  public searchCriteria: SearchCriteria;
+  public abstract defaultPopulationArgument: object;
 
   public getId(request: Request): string {
     return request && request.params ? request.params['id'] : null;
@@ -42,11 +39,17 @@ export abstract class BaseController<IModelMongooseComposite extends Document>{
 
   public single(request: Request, response: Response, next: NextFunction): Promise<IModelMongooseComposite> {
     let query = this.mongooseModelInstance
-      .findById(this.getId(request))
+      .findById(this.getId(request));
 
     query = this.defaultPopulationArgument ? query.populate(this.defaultPopulationArgument) : query;
 
     return query.then((item) => {
+      if (!item) {
+        let error = new Error('Item Not Found');
+        error['status'] = 404;
+        throw (error);
+      }
+      
       response.json(item);
       log.info(`Executed Single Operation: ${this.mongooseModelInstance.collection.name}, item._id: ${item._id}`);
       return item;
@@ -58,17 +61,21 @@ export abstract class BaseController<IModelMongooseComposite extends Document>{
     response.json(new this.mongooseModelInstance());
   }
 
+  public utility(request: Request, response: Response, next: NextFunction): void {
+    response.json({});
+  }
+
   public count(request: Request, response: Response, next: NextFunction): Promise<void> {
     this.searchCriteria = new SearchCriteria(request, next);
     return this.mongooseModelInstance
       .find(this.searchCriteria.criteria)
       .count()
-      .then((count: Number) => {
+      .then((count: number) => {
 
         response.json({
           CollectionName: this.mongooseModelInstance.collection.name,
           CollectionCount: count,
-          SearchCriteria: this.searchCriteria.criteria
+          SearchCriteria: this.searchCriteria.criteria,
         });
 
         log.info(`Executed Count Operation: ${this.mongooseModelInstance.collection.name}, Count: ${count}`);
@@ -88,18 +95,34 @@ export abstract class BaseController<IModelMongooseComposite extends Document>{
       .catch((error) => { next(error); });
   }
 
-  public update(request: Request, response: Response, next: NextFunction): Promise<IModelMongooseComposite> {
+  // For now update full/partial do exactly the same thing, whenever we want to break out
+  // patch, we can do that.
+  public updateFull(request: Request, response: Response, next: NextFunction): Promise<IModelMongooseComposite> {
+    return this.update(request, response, next);
+  }
 
+  public updatePartial(request: Request, response: Response, next: NextFunction): Promise<IModelMongooseComposite> {
+    return this.update(request, response, next);
+  }
+
+  private update(request: Request, response: Response, next: NextFunction): Promise<IModelMongooseComposite> {
     return this.mongooseModelInstance
-      .findByIdAndUpdate(this.getId(request), new this.mongooseModelInstance(request.body), { new: true })
-      .then((createdItem: IModelMongooseComposite) => {
+      .findByIdAndUpdate(this.getId(request), new this.mongooseModelInstance(request.body), { new: false })
+      .then((updatedItem: IModelMongooseComposite) => {
+        if (!updatedItem) {
+          let error = new Error('Item Not Found');
+          error['status'] = 404;
+          throw (error);
+        }
 
-        response.json(createdItem);
+        response.json(updatedItem);
 
-        log.info(`Updated a: ${this.mongooseModelInstance.collection.name}, ID: ${createdItem._id}`);
-        return createdItem;
+        log.info(`Updated a: ${this.mongooseModelInstance.collection.name}, ID: ${updatedItem._id}`);
+        return updatedItem;
       })
-      .catch((error) => { next(error); });
+      .catch((error) => {
+        next(error);
+      });
   }
 
   public destroy(request: Request, response: Response, next: NextFunction): Promise<IModelMongooseComposite> {
@@ -109,30 +132,35 @@ export abstract class BaseController<IModelMongooseComposite extends Document>{
     query = this.defaultPopulationArgument ? query.populate(this.defaultPopulationArgument) : query;
 
     return query.then((deletedItem) => {
+      if (!deletedItem) {
+        let error = new Error('Item Not Found');
+        error['status'] = 404;
+        throw (error);
+      }
 
-        response.json({
-          ItemRemovedId: this.getId(request),
-          ItemRemoved: deletedItem
-        });
+      response.json({
+        ItemRemovedId: this.getId(request),
+        ItemRemoved: deletedItem,
+      });
 
-        log.info(`Removed a: ${this.mongooseModelInstance.collection.name}, ID: ${this.getId(request)}`);
-        return deletedItem;
-      })
+      log.info(`Removed a: ${this.mongooseModelInstance.collection.name}, ID: ${this.getId(request)}`);
+      return deletedItem;
+    })
       .catch((error) => { next(error); });
   }
 
   public query(request: Request, response: Response, next: NextFunction): Promise<IModelMongooseComposite[]> {
-    let query = this.mongooseModelInstance.find(request.body)
-    
-     query = this.defaultPopulationArgument ? query.populate(this.defaultPopulationArgument) : query;
+    let query = this.mongooseModelInstance.find(request.body);
 
-     return query.then((items: IModelMongooseComposite[]) => {
+    query = this.defaultPopulationArgument ? query.populate(this.defaultPopulationArgument) : query;
 
-        response.json({ items });
+    return query.then((items: IModelMongooseComposite[]) => {
 
-        log.info(`Queried for: ${this.mongooseModelInstance.collection.name}, Found: ${items.length}`);
-        return items;
-      })
+      response.json({ items });
+
+      log.info(`Queried for: ${this.mongooseModelInstance.collection.name}, Found: ${items.length}`);
+      return items;
+    })
       .catch((error) => { next(error); });
   }
 }
